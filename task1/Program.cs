01e0590bb82a6b30d4e257a5273982947f5dde46
diplg <dimas4354@icloud.com>
2022-04-23 14:53:29 +0300

    задача 1

---
+++ task1/Program.cs
@@ -0,0 +1,26 @@
+﻿// задача 1
+int X, Y;
+X = new Random().Next(-100, 100);
+Y = new Random().Next(-100, 100);
+Console.WriteLine($" X = {X}, Y = {Y}");
+
+if (X == 0 | Y == 0)
+{
+    Console.WriteLine("Не пренадлежит какой либо конкретной плоскости");
+}
+else if (X > 0 & Y > 0)
+{
+    Console.WriteLine("Пренадлежит плоскости 1");
+}
+else if (X > 0 & Y < 0)
+{
+    Console.WriteLine("Пренадлежит плоскости 2");
+}
+else if (X < 0 & Y < 0)
+{
+    Console.WriteLine("Пренадлежит плоскости 3");
+}
+else if (X < 0 & Y > 0)
+{
+    Console.WriteLine("Пренадлежит плоскости 4");
+}
