3a109d9fba363ebfee5bc7fb67fb486050df253f
diplg <dimas4354@icloud.com>
2022-04-23 15:59:29 +0300

    задание 3

---
+++ task3/Program.cs
@@ -0,0 +1,8 @@
+﻿// задание 3 ищем растояние между двумя точками 
+int x1 = 3;
+int y1 = 6;
+int x2 = 2;
+int y2 = 1;
+
+int length = (int)Math.Sqrt(Math.Pow (x1-x2,2) + Math.Pow (y1-y2, 2));
+Console.WriteLine(length);
